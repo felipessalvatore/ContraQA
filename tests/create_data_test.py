8fcf74ebee59e2a59bab69de55536234d4ed2687
felipessalvatore <felipessalvatore@github.com>
2018-08-20 12:51:06 -0300

    add data 5 and fix tests

--- tests/create_data_test.py
+++ tests/create_data_test.py
@@ -5,6 +5,7 @@ from contra_qa.text_generation.boolean1_neg import boolean1
 from contra_qa.text_generation.boolean2_S_and import boolean2
 from contra_qa.text_generation.boolean3_NP_and import boolean3
 from contra_qa.text_generation.boolean4_VP_and import boolean4
+from contra_qa.text_generation.boolean5_AP_and import boolean5
 
 
 class AddDataset(unittest.TestCase):
@@ -20,24 +21,35 @@ class AddDataset(unittest.TestCase):
 
     def test_generate_boolean1(self):
         boolean1()
-        cond = os.path.join("data", "boolean1_test.csv")
-        cond = cond and os.path.join("data", "boolean1_train.csv")
+        path_train = os.path.join("data", "boolean1_train.csv")
+        path_test = os.path.join("data", "boolean1_test.csv")
+        cond = os.path.exists(path_train) and os.path.exists(path_test)
         self.assertTrue(cond)
 
     def test_generate_boolean2(self):
         boolean2()
-        cond = os.path.join("data", "boolean2_test.csv")
-        cond = cond and os.path.join("data", "boolean2_train.csv")
+        path_train = os.path.join("data", "boolean2_train.csv")
+        path_test = os.path.join("data", "boolean2_test.csv")
+        cond = os.path.exists(path_train) and os.path.exists(path_test)
         self.assertTrue(cond)
 
     def test_generate_boolean3(self):
         boolean3()
-        cond = os.path.join("data", "boolean3_test.csv")
-        cond = cond and os.path.join("data", "boolean3_train.csv")
+        path_train = os.path.join("data", "boolean3_train.csv")
+        path_test = os.path.join("data", "boolean3_test.csv")
+        cond = os.path.exists(path_train) and os.path.exists(path_test)
         self.assertTrue(cond)
 
     def test_generate_boolean4(self):
         boolean4()
-        cond = os.path.join("data", "boolean4_test.csv")
-        cond = cond and os.path.join("data", "boolean4_train.csv")
+        path_train = os.path.join("data", "boolean4_train.csv")
+        path_test = os.path.join("data", "boolean4_test.csv")
+        cond = os.path.exists(path_train) and os.path.exists(path_test)
+        self.assertTrue(cond)
+
+    def test_generate_boolean5(self):
+        boolean5()
+        path_train = os.path.join("data", "boolean5_train.csv")
+        path_test = os.path.join("data", "boolean5_test.csv")
+        cond = os.path.exists(path_train) and os.path.exists(path_test)
         self.assertTrue(cond)
