05ef71f0ca551e186c29485b3f8301a7c11b10cd
felipessalvatore <felipessalvatore@github.com>
2018-08-21 20:22:09 -0300

    add install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt-get update 
+pip3 install numpy
+pip3 install pandas
+pip3 install seaborn
+pip3 install scikit-learn
+pip3 install matplotlib
+pip3 install -U spacy
+pip3 install http://download.pytorch.org/whl/cpu/torch-0.4.1-cp35-cp35m-linux_x86_64.whl 
+pip3 install torchtext
+python3 -m spacy download en
+python3 setup.py test
