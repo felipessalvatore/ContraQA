764664b177a2fb08ed85ee95e4777951e3d85b83
felipessalvatore <felipessalvatore@github.com>
2018-09-28 23:43:53 -0300

    update script

--- control_script.sh
+++ control_script.sh
@@ -1,4 +1,3 @@
-python3 -u search_classification_plus.py
-python3 -u search_classification_plus.py -m gru
-python3 -u search_classification_plus.py -m lstm
-
+python3 -u search_classification_plus.py -st 15 -rt 8
+python3 -u search_classification_plus.py -m gru -st 15 -rt 8
+python3 -u search_classification_plus.py -m lstm -st 15 -rt 8
